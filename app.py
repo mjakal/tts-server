c66551d59823c5d65de92c2d232f1a00133c9120
Martin Jakal <mjakal@gmail.com>
2019-02-18 21:51:27 +0100

    basic implementation done

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import pyttsx
-from flask import Flask, render_template, g
-# from flask import g
+from flask import Flask, render_template, request, jsonify, g
 
 app = Flask(__name__)
+
 
 def get_engine():
     engine = getattr(g, '_engine', None)
@@ -10,12 +10,29 @@ def get_engine():
         engine = g._engine = pyttsx.init()
     return engine
 
+
 @app.route('/')
 def index():
     # engine = get_engine()
     # engine.say("testing this shit")
     # engine.runAndWait()
     return render_template("index.html")
+
+
+@app.route('/speak', methods=["POST"])
+def speak():
+    voice = request.form["voice"]
+    text = request.form["text"]
+
+    if voice and text:
+        engine = get_engine()
+        engine.say(text)
+        engine.runAndWait()
+
+        return jsonify({'success': 'valid data'})
+    
+    return jsonify({'error': 'missing data'})
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
